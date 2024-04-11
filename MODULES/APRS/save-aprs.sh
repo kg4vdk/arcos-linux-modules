2aaf363cfb04d3eddfcf3628846139ffb07b2d59
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-04-10 23:24:37 -0500

    Delete MODULES directory

--- MODULES/APRS/save-aprs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp -r $HOME/.java/.userPrefs/org/ka2ddo/yaac/Profiles/arcOS /tmp/aprs-profile
-
-tar -C /tmp -czf aprs-profile_$(date +"%Y%m%d%H%M").tgz aprs-profile
-
-rm -rf /tmp/aprs-profile
