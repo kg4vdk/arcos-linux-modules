31701fa0f6c91bc956e16d4396dc0eb11a403b37
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-07-20 04:28:52 +0000

    2024-07-19
    Delete COMMUNITY/APPEARANCE directory

--- COMMUNITY/APPEARANCE/00_CLOCK-SHOW-SECONDS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jq '."custom-format"."value" = "%a, %e %b%n%H:%M:%S %Z"' ~/.config/cinnamon/spices/calendar@cinnamon.org/13.json > /tmp/13.json && mv /tmp/13.json ~/.config/cinnamon/spices/calendar@cinnamon.org/13.json
