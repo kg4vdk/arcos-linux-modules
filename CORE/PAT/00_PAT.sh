db306684abb21f86a11398129284127e6b630d9c
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-07-25 20:46:43 +0000

    Add files via upload

--- CORE/PAT/00_PAT.sh
+++ CORE/PAT/00_PAT.sh
@@ -17,27 +17,30 @@ MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
 ########################
 
 ### MODULE COMMANDS FUNCTION ###
 module_commands () {
 
-if [ -f $MODULE_DIR/config.json ]; then
-	cp $MODULE_DIR/config.json $HOME/.config/pat/config.json
+mkdir -p $SAVE_DIR
+
+if [ -f $SAVE_DIR/config.json ]; then
+	cp $SAVE_DIR/config.json $HOME/.config/pat/config.json
 fi
 
-if [ -f $MODULE_DIR/rmslist.json ]; then
-	ln -sf $MODULE_DIR/rmslist.json $HOME/.local/share/pat/rmslist.json
+if [ -f $SAVE_DIR/rmslist.json ]; then
+	ln -sf $SAVE_DIR/rmslist.json $HOME/.local/share/pat/rmslist.json
 else
-	cp $HOME/.local/share/pat/rmslist.json $MODULE_DIR/rmslist.json
+	cp $HOME/.local/share/pat/rmslist.json $SAVE_DIR/rmslist.json
 	rm $HOME/.local/share/pat/rmslist.json
-	ln -s $MODULE_DIR/rmslist.json $HOME/.local/share/pat/rmslist.json
+	ln -s $SAVE_DIR/rmslist.json $HOME/.local/share/pat/rmslist.json
 fi
 
 for mailbox in archive in out sent; do
     rm -rf $HOME/.local/share/pat/mailbox/$MYCALL/$mailbox
-    mkdir -p $MODULE_DIR/$mailbox
-    ln -sTf $MODULE_DIR/$mailbox $HOME/.local/share/pat/mailbox/$MYCALL/$mailbox
+    mkdir -p $SAVE_DIR/$mailbox
+    ln -sTf $SAVE_DIR/$mailbox $HOME/.local/share/pat/mailbox/$MYCALL/$mailbox
 done
 
 sudo systemctl restart pat@$USER.service
