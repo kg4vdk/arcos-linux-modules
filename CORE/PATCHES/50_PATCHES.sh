a660571fe3dae1856ab6546d31b28199405321df
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-01 13:58:12 +0000

    Add files via upload

---
+++ CORE/PATCHES/50_PATCHES.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+######################
+# PATCHES QRV MODULE #
+######################
+MODULE="PATCHES"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/media/$USER/ARCOS-DATA
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+###########################################################################
+
+
+
+######################
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
