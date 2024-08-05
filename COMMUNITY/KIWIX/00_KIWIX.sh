20c1084b06a57b2a2c8527f957a082d1e817acc0
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-05 04:37:34 +0000

    Add files via upload

---
+++ COMMUNITY/KIWIX/00_KIWIX.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+####################
+# KIWIX QRV MODULE #
+####################
+MODULE="KIWIX"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/media/$USER/ARCOS-DATA
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/COMMUNITY/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+mkdir -p $SAVE_DIR
+
+$MODULE_DIR/kiwix-tools/kiwix-serve -d -p 1234 -M $SAVE_DIR/zim/*
+
+cp $MODULE_DIR/applications/kiwix.desktop $HOME/Desktop/
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
