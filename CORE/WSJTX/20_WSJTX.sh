fa9d1f0377b06f55ec928e1c7566680ea8fe0422
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-27 19:47:13 +0000

    Add files via upload

--- CORE/WSJTX/20_WSJTX.sh
+++ CORE/WSJTX/20_WSJTX.sh
@@ -12,12 +12,18 @@ MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
 MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
 MYQTH="${MYCITY}, ${MYST}"
 MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
 
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
 SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
 ########################
 
 ### MODULE COMMANDS FUNCTION ###
@@ -27,8 +33,10 @@ sudo cp $MODULE_DIR/save-wsjtx.sh /opt/arcOS/bin/
 cp $MODULE_DIR/save-wsjtx.desktop $HOME/.local/share/applications/
 mkdir -p $SAVE_DIR
 
-if [ -f $SAVE_DIR/WSJT-X.ini ]; then
-	cp $SAVE_DIR/WSJT-X.ini $HOME/.config/WSJT-X.ini
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini ]; then
+	cp $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini $HOME/.config/WSJT-X.ini
+elif [ -f $QRV_PROFILE_DIR/DEFAULT/$MODULE/WSJT-X_DEFAULT.ini ]; then
+	cp $QRV_PROFILE_DIR/DEFAULT/$MODULE/WSJT-X_DEFAULT.ini $HOME/.config/WSJT-X.ini
 fi
 
 mkdir -p $SAVE_DIR/WSJT-X
--- CORE/WSJTX/save-wsjtx.sh
+++ CORE/WSJTX/save-wsjtx.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+####################
+# WSJTX QRV MODULE #
+####################
 MODULE="WSJTX"
 
 # STATION INFO
@@ -13,21 +16,38 @@ MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
-SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
-
-mkdir -p $SAVE_DIR
+LOGFILE=$MODULE_DIR/$MODULE.log
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
 
 wsjtx_save () {
-if [ -f $SAVE_DIR/WSJT-X.ini ]; then
-	mv $SAVE_DIR/WSJT-X.ini $SAVE_DIR/WSJT-X_$(date +"%Y%m%d%H%M%Z").ini
-	cp $HOME/.config/WSJT-X.ini $SAVE_DIR/WSJT-X.ini
+QRV_PROFILE=$(echo "${QRV_PROFILE}" | tr '[:lower:]' '[:upper:]' | sed 's/|//')
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini ]; then
+	mv $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}_$(date +"%F_%H%M").ini
+	cp $HOME/.config/WSJT-X.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini
 else
-	cp $HOME/.config/WSJT-X.ini $SAVE_DIR/WSJT-X.ini
+	mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+	cp $HOME/.config/WSJT-X.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/WSJT-X_${QRV_PROFILE}.ini
 fi
 }
 
-if wsjtx_save; then
-	notify-send --icon=wsjtx-custom-icon "WSJTX" "Configuration saved!"
-else
-	notify-send --icon=error "WSJTX" "Error saving configuration!"
+QRV_PROFILE_LIST="^DEFAULT$(for profile in $(ls /media/$USER/ARCOS-DATA/QRV/${MYCALL}/SAVED/PROFILES 2> /dev/null); do echo -n "!$profile"; done)"
+
+QRV_PROFILE=$(yad --title="Save to QRV Profile" \
+--window-icon="checkbox-qt" \
+--form --borders=36 \
+--center \
+--fixed \
+--field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
+--text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
+--buttons-layout=end \
+--undecorated)
+
+if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
+	if wsjtx_save; then
+		notify-send --icon=wsjtx-custom-icon "WSJTX" "Configuration saved!"
+	else
+		notify-send --icon=error "WSJTX" "Error saving configuration!"
+	fi
 fi
