23c132d569281d75e76852c13edfacf87c50a069
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-27 19:46:55 +0000

    Add files via upload

--- CORE/PAT/20_PAT.sh
+++ CORE/PAT/20_PAT.sh
@@ -12,12 +12,18 @@ MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
 MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
 MYQTH="${MYCITY}, ${MYST}"
 MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
 
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
 SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
 ########################
 
 ### MODULE COMMANDS FUNCTION ###
@@ -27,8 +33,10 @@ sudo cp $MODULE_DIR/save-pat.sh /opt/arcOS/bin/
 cp $MODULE_DIR/save-pat.desktop $HOME/.local/share/applications/
 mkdir -p $SAVE_DIR
 
-if [ -f $SAVE_DIR/config.json ]; then
-	cp $SAVE_DIR/config.json $HOME/.config/pat/config.json
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json ]; then
+	cp $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json $HOME/.config/pat/config.json
+elif [ -f $QRV_PROFILE_DIR/DEFAULT/$MODULE/config_DEFAULT.json ]; then
+	cp $QRV_PROFILE_DIR/DEFAULT/$MODULE/config_DEFAULT.json $HOME/.config/pat/config.json
 fi
 
 if [ -f $SAVE_DIR/rmslist.json ]; then
--- CORE/PAT/save-pat.sh
+++ CORE/PAT/save-pat.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##################
+# PAT QRV MODULE #
+##################
 MODULE="PAT"
 
 # STATION INFO
@@ -13,22 +16,40 @@ MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
-SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
 
 mkdir -p $SAVE_DIR
 
 pat_save () {
-if [ -f $SAVE_DIR/config.json ]; then
-	mv $SAVE_DIR/config.json  $SAVE_DIR/config_$(date +"%Y%m%d%H%M%Z").json
-	cp $HOME/.config/pat/config.json $SAVE_DIR/config.json 
+QRV_PROFILE=$(echo "${QRV_PROFILE}" | tr '[:lower:]' '[:upper:]' | sed 's/|//')
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json ]; then
+	mv $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json  $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}_$(date +"%F_%H%M").json
+	cp $HOME/.config/pat/config.json $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json 
 else
-	cp $HOME/.config/pat/config.json $SAVE_DIR/config.json
+	mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+	cp $HOME/.config/pat/config.json $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/config_${QRV_PROFILE}.json
 fi
 }
 
-if pat_save; then
-	notify-send --icon=pat-custom-icon "PAT WLNK" "Configuration saved!"
-else
-	notify-send --icon=error "PAT WLNK" "Error saving configuration!"
-fi
+QRV_PROFILE_LIST="^DEFAULT$(for profile in $(ls /media/$USER/ARCOS-DATA/QRV/${MYCALL}/SAVED/PROFILES 2> /dev/null); do echo -n "!$profile"; done)"
 
+QRV_PROFILE=$(yad --title="Save to QRV Profile" \
+--window-icon="checkbox-qt" \
+--form --borders=36 \
+--center \
+--fixed \
+--field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
+--text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
+--buttons-layout=end \
+--undecorated)
+
+if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
+	if pat_save; then
+		notify-send --icon=pat-custom-icon "PAT WLNK" "Configuration saved!"
+	else
+		notify-send --icon=error "PAT WLNK" "Error saving configuration!"
+	fi
+fi
