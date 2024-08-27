1b7f7b81be24ae21e74df2d0a9f0b4fb3cb90d66
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-27 19:46:39 +0000

    Add files via upload

--- CORE/JS8CALL/20_JS8CALL.sh
+++ CORE/JS8CALL/20_JS8CALL.sh
@@ -12,12 +12,18 @@ MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
 MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
 MYQTH="${MYCITY}, ${MYST}"
 MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
 
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
 SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
 ########################
 
 ### MODULE COMMANDS FUNCTION ###
@@ -27,8 +33,10 @@ sudo cp $MODULE_DIR/save-js8call.sh /opt/arcOS/bin/
 cp $MODULE_DIR/save-js8call.desktop $HOME/.local/share/applications/
 mkdir -p $SAVE_DIR
 
-if [ -f $SAVE_DIR/JS8Call.ini ]; then
-	cp $SAVE_DIR/JS8Call.ini $HOME/.config/JS8Call.ini
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini ]; then
+	cp $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini $HOME/.config/JS8Call.ini
+elif [ -f $QRV_PROFILE_DIR/DEFAULT/$MODULE/JS8Call_DEFAULT.ini ]; then
+	cp $QRV_PROFILE_DIR/DEFAULT/$MODULE/JS8Call_DEFAULT.ini $HOME/.config/JS8Call.ini
 fi
 
 mkdir -p $SAVE_DIR/JS8Call
--- CORE/JS8CALL/save-js8call.sh
+++ CORE/JS8CALL/save-js8call.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+######################
+# JS8CALL QRV MODULE #
+######################
 MODULE="JS8CALL"
 
 # STATION INFO
@@ -13,21 +16,38 @@ MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
-SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
-
-mkdir -p $SAVE_DIR
+LOGFILE=$MODULE_DIR/$MODULE.log
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
 
 js8call_save () {
-if [ -f $SAVE_DIR/JS8Call.ini ]; then
-	mv $SAVE_DIR/JS8Call.ini $SAVE_DIR/JS8Call_$(date +"%Y%m%d%H%M%Z").ini
-	cp $HOME/.config/JS8Call.ini $SAVE_DIR/JS8Call.ini
+QRV_PROFILE=$(echo "${QRV_PROFILE}" | tr '[:lower:]' '[:upper:]' | sed 's/|//')
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini ]; then
+	mv $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}_$(date +"%F_%H%M").ini
+	cp $HOME/.config/JS8Call.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini
 else
-	cp $HOME/.config/JS8Call.ini $SAVE_DIR/JS8Call.ini
+	mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+	cp $HOME/.config/JS8Call.ini $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/JS8Call_${QRV_PROFILE}.ini
 fi
 }
 
-if js8call_save; then
-	notify-send --icon=js8call-custom-icon "JS8CALL" "Configuration saved!"
-else
-	notify-send --icon=error "JS8CALL" "Error saving configuration!"
+QRV_PROFILE_LIST="^DEFAULT$(for profile in $(ls /media/$USER/ARCOS-DATA/QRV/${MYCALL}/SAVED/PROFILES 2> /dev/null); do echo -n "!$profile"; done)"
+
+QRV_PROFILE=$(yad --title="Save to QRV Profile" \
+--window-icon="checkbox-qt" \
+--form --borders=36 \
+--center \
+--fixed \
+--field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
+--text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
+--buttons-layout=end \
+--undecorated)
+
+if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
+	if js8call_save; then
+		notify-send --icon=js8call-custom-icon "JS8CALL" "Configuration saved!"
+	else
+		notify-send --icon=error "JS8CALL" "Error saving configuration!"
+	fi
 fi
