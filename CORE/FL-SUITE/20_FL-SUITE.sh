6418383ca51cf7cd41b363046728e464e194acea
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-27 19:46:12 +0000

    Add files via upload

--- CORE/FL-SUITE/20_FL-SUITE.sh
+++ CORE/FL-SUITE/20_FL-SUITE.sh
@@ -12,12 +12,18 @@ MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
 MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
 MYQTH="${MYCITY}, ${MYST}"
 MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
 
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
 SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
 ########################
 
 ### MODULE COMMANDS FUNCTION ###
@@ -27,7 +33,11 @@ sudo cp $MODULE_DIR/save-fl-suite.sh /opt/arcOS/bin/
 cp $MODULE_DIR/save-fl-suite.desktop $HOME/.local/share/applications/
 mkdir -p $SAVE_DIR
 
-FL_SUITE_CONFIG=$SAVE_DIR/fl-suite.tgz
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz ]; then
+	FL_SUITE_CONFIG=$QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz
+else
+	FL_SUITE_CONFIG=$QRV_PROFILE_DIR/DEFAULT/$MODULE/fl-suite_DEFAULT.tgz
+fi
 
 if [ -f $FL_SUITE_CONFIG ]; then
 	tar -C $HOME -xzf $FL_SUITE_CONFIG
--- CORE/FL-SUITE/save-fl-suite.sh
+++ CORE/FL-SUITE/save-fl-suite.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#######################
+# FL-SUITE QRV MODULE #
+#######################
 MODULE="FL-SUITE"
 
 # STATION INFO
@@ -13,29 +16,47 @@ MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 # PATHS
 ARCOS_DATA=/media/$USER/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
-SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
-
-mkdir -p $SAVE_DIR
+LOGFILE=$MODULE_DIR/$MODULE.log
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
 
 fl_suite_save () {
-if [ -f $SAVE_DIR/fl-suite.tgz ]; then
-	mv $SAVE_DIR/fl-suite.tgz $SAVE_DIR/fl-suite_$(date +"%Y%m%d%H%M%Z").tgz
+QRV_PROFILE=$(echo "${QRV_PROFILE}" | tr '[:lower:]' '[:upper:]' | sed 's/|//')
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz ]; then
+	mv $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}_$(date +"%F_%H%M").tgz
 	if [ -d $HOME/.flrig ]; then
-		tar -C $HOME -czf $SAVE_DIR/fl-suite.tgz .fldigi .nbems .flrig
+		tar -C $HOME -czf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz .fldigi .nbems .flrig
 	else
-		tar -C $HOME -czf $SAVE_DIR/fl-suite.tgz .fldigi .nbems
+		tar -C $HOME -czf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz .fldigi .nbems
 	fi
 else
 	if [ -d $HOME/.flrig ]; then
-		tar -C $HOME -czf $SAVE_DIR/fl-suite.tgz .fldigi .nbems .flrig
+		mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+		tar -C $HOME -czf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz .fldigi .nbems .flrig
 	else
-		tar -C $HOME -czf $SAVE_DIR/fl-suite.tgz .fldigi .nbems
+		mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+		tar -C $HOME -czf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/fl-suite_${QRV_PROFILE}.tgz .fldigi .nbems
 	fi
 fi
 }
 
-if fl_suite_save; then
-	notify-send --icon=fldigi-custom-icon "FL-SUITE" "Configuration saved!"
-else
-	notify-send --icon=error "FL-SUITE" "Error saving configuration!"
+QRV_PROFILE_LIST="^DEFAULT$(for profile in $(ls /media/$USER/ARCOS-DATA/QRV/${MYCALL}/SAVED/PROFILES 2> /dev/null); do echo -n "!$profile"; done)"
+
+QRV_PROFILE=$(yad --title="Save to QRV Profile" \
+--window-icon="checkbox-qt" \
+--form --borders=36 \
+--center \
+--fixed \
+--field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
+--text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
+--buttons-layout=end \
+--undecorated)
+
+if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
+	if fl_suite_save; then
+		notify-send --icon=fldigi-custom-icon "FL-SUITE" "Configuration saved!"
+	else
+		notify-send --icon=error "FL-SUITE" "Error saving configuration!"
+	fi
 fi
