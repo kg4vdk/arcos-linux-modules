fbf5e4263ca979ab82c7af4e69c4b3ebae9862b6
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-08-31 23:16:45 -0500

    Update save-direwolf.sh

--- CORE/DIREWOLF/save-direwolf.sh
+++ CORE/DIREWOLF/save-direwolf.sh
@@ -40,8 +40,7 @@ QRV_PROFILE=$(yad --title="Save to QRV Profile" \
 --fixed \
 --field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
 --text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
---buttons-layout=end \
---undecorated)
+--buttons-layout=end)
 
 if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
 	if direwolf_save; then
