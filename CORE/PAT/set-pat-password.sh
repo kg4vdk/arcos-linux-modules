ad59d221104f6070915836e8186082755d946f82
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2024-10-10 11:06:12 -0500

    add notification

--- CORE/PAT/set-pat-password.sh
+++ CORE/PAT/set-pat-password.sh
@@ -15,6 +15,8 @@ PAT_PASSWORD=$(echo -n "$PAT_PASSWORD" | sed 's/|//')
 sed -i 's/^.*"secure_login_password": ".*",$/  "secure_login_password": "'"$PAT_PASSWORD"'",/' $HOME/.config/pat/config.json
 
 sudo systemctl restart pat@user.service
+
+notify-send --icon=pat-custom-icon "PAT WLNK" "Password set!\n\nTo save the password persistently, use 'Menu > arcOS Tools > SAVE PAT WLNK CONFIG'."
 }
 
 save_pat_password
