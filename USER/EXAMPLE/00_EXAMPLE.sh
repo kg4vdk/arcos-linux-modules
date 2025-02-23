02e33a9d2f1ac24852a955be851d373f0dfc8b0e
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-02-23 11:00:13 -0600

    Update 00_EXAMPLE.sh

--- USER/EXAMPLE/00_EXAMPLE.sh
+++ USER/EXAMPLE/00_EXAMPLE.sh
@@ -15,7 +15,7 @@ MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
 MYQTH="${MYCITY}, ${MYST}"
 MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
 
-# PATHS (Defines paths referencein the module)
+# PATHS (Defines paths referenced in the module)
 ARCOS_DATA=/ARCOS-DATA
 MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/USER/$MODULE
 LOGFILE=$MODULE_DIR/$MODULE.log
