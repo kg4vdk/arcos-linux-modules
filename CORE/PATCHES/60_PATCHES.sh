883e90ff10ee5340f0684b6b1c0fd26f03ee1c91
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-02-23 10:55:42 -0600

    Update 60_PATCHES.sh

--- CORE/PATCHES/60_PATCHES.sh
+++ CORE/PATCHES/60_PATCHES.sh
@@ -24,13 +24,6 @@ module_commands () {
 
 ###########################################################################
 
-sudo cp $MODULE_DIR/config/lighttpd.conf /etc/lighttpd/
-sudo cp $MODULE_DIR/config/89-skyaware.conf /etc/lighttpd/conf-available/
-sudo systemctl daemon-reload
-sudo systemctl restart lighttpd.service
-
-cp $MODULE_DIR/applications/hamrs.desktop $HOME/.local/share/applications/
-chmod +x $HOME/.local/share/applications/hamrs.desktop
 
 
 ######################
