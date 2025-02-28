a8b846e809f812c6eec01b54a36c382b13d9b053
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-02-28 02:14:35 +0000

    Add QSSTV module

---
+++ CORE/QSSTV/20_QSSTV.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+####################
+# QSSTV QRV MODULE #
+####################
+MODULE="QSSTV"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
+
+# PATHS
+ARCOS_DATA=/ARCOS-DATA
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+sudo cp $MODULE_DIR/save-qsstv.sh /opt/arcOS/bin/
+cp $MODULE_DIR/save-qsstv.desktop $HOME/.local/share/applications/
+mkdir -p $SAVE_DIR
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf ]; then
+	cp $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf $HOME/.config/ON4QZ/qsstv_9.0.conf
+elif [ -f $QRV_PROFILE_DIR/DEFAULT/$MODULE/qsstv_9.0_DEFAULT.conf ]; then
+	cp $QRV_PROFILE_DIR/DEFAULT/$MODULE/qsstv_9.0_DEFAULT.conf $HOME/.config/ON4QZ/qsstv_9.0.conf
+fi
+
+for directory in audio rx tx templates; do
+	mkdir -p $SAVE_DIR/$directory
+   	unlink $HOME/.qsstv/$directory 2> /dev/null
+   	rm -rf $HOME/.qsstv/$directory    	
+	ln -sTf $SAVE_DIR/$directory $HOME/.qsstv/$directory
+done
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
---
+++ CORE/QSSTV/save-qsstv.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+####################
+# QSSTV QRV MODULE #
+####################
+MODULE="QSSTV"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/ARCOS-DATA
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
+
+mkdir -p $SAVE_DIR
+
+qsstv_save () {
+QRV_PROFILE=$(echo "${QRV_PROFILE}" | tr '[:lower:]' '[:upper:]' | sed 's/|//')
+
+if [ -f $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf ]; then
+	mv $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf  $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}_$(date +"%F_%H%M").conf
+	cp $HOME/.config/ON4QZ/qsstv_9.0.conf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf 
+else
+	mkdir -p $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE
+	cp $HOME/.config/ON4QZ/qsstv_9.0.conf $QRV_PROFILE_DIR/${QRV_PROFILE}/$MODULE/qsstv_9.0_${QRV_PROFILE}.conf
+fi
+}
+
+QRV_PROFILE_LIST="^DEFAULT$(for profile in $(ls /ARCOS-DATA/QRV/${MYCALL}/SAVED/PROFILES 2> /dev/null); do echo -n "!$profile"; done)"
+
+QRV_PROFILE=$(yad --title="Save to QRV Profile" \
+--window-icon="checkbox-qt" \
+--form --borders=36 \
+--center \
+--fixed \
+--field="QRV Profile":CBE "${QRV_PROFILE_LIST}" \
+--text="Select (or create) the QRV Profile where the configuration will be saved.\n" \
+--buttons-layout=end)
+
+if echo "${QRV_PROFILE}" | grep "|" > /dev/null; then
+	if qsstv_save; then
+		notify-send --icon=qsstv-custom-icon "QSSTV" "Configuration saved!"
+	else
+		notify-send --icon=error "QSSTV" "Error saving configuration!"
+	fi
+fi
