881bebb9c79e8a21dbf328be121e108e5517967d
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-03-03 21:44:47 +0000

    Add files via upload

---
+++ CORE/CALENDAR/30_CALENDAR.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#######################
+# CALENDAR QRV MODULE #
+#######################
+MODULE="CALENDAR"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+QRV_PROFILE=$(head -n 7 $HOME/.station-info | tail -n 1)
+
+if [ ${QRV_PROFILE} == ${MYLOC} ]; then
+	QRV_PROFILE="NONE"
+fi
+
+# PATHS
+ARCOS_DATA=/ARCOS-DATA
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MODULE
+QRV_PROFILE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/PROFILES
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+# Create system calendar directories
+mkdir -p $SAVE_DIR/{sources,calendar}
+# Create user specific calendar directories
+mkdir -p $ARCOS_DATA/QRV/$MYCALL/SAVED/CALENDAR/
+
+# Remove existing source files
+rm $HOME/.config/evolution/sources/*.source
+
+# Copy calendar source files into place
+cp $MODULE_DIR/sources/* $SAVE_DIR/sources/
+
+TIMESTAMP=$(date +"%FT%R:%S")
+
+# Set system calendar revision to timestamp
+sed -i "s/Revision=.*$/Revision=$TIMESTAMP/" $SAVE_DIR/sources/arcOS.source
+
+# Set user calendar revision to timestamp
+sed -i "s/Revision=.*$/Revision=$TIMESTAMP/" $SAVE_DIR/sources/user.source
+
+# Set user calendar name to callsign
+sed -i "s/XXXCALLSIGNXXX/$MYCALL/g" $SAVE_DIR/sources/user.source
+
+# Set the evolution autoconfig directory
+gsettings set org.gnome.evolution-data-server autoconfig-directory '/ARCOS-DATA/QRV/CALENDAR/sources'
+
+# Restart the cinnamon calendar server
+cinnamon-calendar-server &
+
+# Restart the evolution source registry
+systemctl --user restart evolution-source-registry
+
+# Restart the evolution calendar factory
+systemctl --user restart evolution-calendar-factory.service
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
