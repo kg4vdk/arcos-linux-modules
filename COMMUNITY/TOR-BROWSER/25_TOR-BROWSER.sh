2dd603954f94c5d658b24123ce0bd4188740c6d0
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-07-13 19:02:15 +0000

    Add TOR-BROWSER module

---
+++ COMMUNITY/TOR-BROWSER/25_TOR-BROWSER.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+##########################
+# TOR-BROWSER QRV MODULE #
+##########################
+MODULE="TOR-BROWSER"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/USER/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+pkill -f firefox.real > /dev/null 2>&1
+
+if [ -f ${MODULE_DIR}/tor-browser/start-tor-browser.desktop ]; then
+	cd ${MODULE_DIR}/tor-browser
+	${MODULE_DIR}/tor-browser/start-tor-browser.desktop --register-app
+fi
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
+
