8ad253e75bce4b7da3fa06dd83175d7a1e149cb2
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-07-13 19:33:36 +0000

    Add YT-DLP module

---
+++ COMMUNITY/YT-DLP/25_YT-DLP.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#####################
+# YT-DLP QRV MODULE #
+#####################
+MODULE="YT-DLP"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/USER/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+if [ -f /usr/bin/yt-dlp ]; then
+	sudo rm /usr/bin/yt-dlp
+fi
+
+if [ -f ${MODULE_DIR}/bin/yt-dlp ]; then
+	sudo ln -sf ${MODULE_DIR}/bin/yt-dlp /usr/bin/yt-dlp
+fi
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
+
