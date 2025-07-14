69c3ea097158887a088eaee6ce10b71e121850df
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-07-14 16:42:27 +0000

    re-numbering

---
+++ CORE/MODULES-PRE/91_MODULES-PRE.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+##########################
+# MODULES-PRE QRV MODULE #
+##########################
+MODULE="MODULES-PRE"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+MYNAME=$(head -n 2 $HOME/.station-info | tail -n 1)
+MYCITY=$(head -n 3 $HOME/.station-info | tail -n 1)
+MYST=$(head -n 4 $HOME/.station-info | tail -n 1)
+MYQTH="${MYCITY}, ${MYST}"
+MYLOC=$(head -n 5 $HOME/.station-info | tail -n 1)
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+for i in $(ls /arcHIVE/QRV/${MYCALL}/arcos-linux-modules/COMMUNITY/*_PRE_*.sh); do
+	MODULE_NAME=$(basename $i)
+    echo "${MODULE_NAME}"
+	bash $i
+done
+
+for i in $(ls /arcHIVE/QRV/${MYCALL}/arcos-linux-modules/USER/*_PRE_*.sh); do
+	MODULE_NAME=$(basename $i)
+    echo "${MODULE_NAME}"
+	bash $i
+done
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
