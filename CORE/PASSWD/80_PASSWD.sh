c6dd06a798123e5b1e7cc3057e0e7cf7ad4b6842
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-09-30 03:43:43 +0000

    Add files via upload

---
+++ CORE/PASSWD/80_PASSWD.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#####################
+# PASSWD QRV MODULE #
+#####################
+MODULE="PASSWD"
+
+# STATION INFO
+MYCALL=$(head -n 1 $HOME/.station-info)
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+### USERS MAY GENERATE A PASSWD FILE BY RUNNING:
+### `openssl passwd -1 supersecretpasword > /arcHIVE/QRV/CALLSIGN/.passwd`
+
+if [ -f $ARCOS_DATA/QRV/$MYCALL/.passwd ]; then
+	sudo usermod --password $(cat $ARCOS_DATA/QRV/$MYCALL/.passwd) user
+fi
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
