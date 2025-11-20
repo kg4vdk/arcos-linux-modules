ecb4a74279217b5e14b830749b7946a85dd2dae1
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-11-20 02:28:29 +0000

    Link Viking's default map to be used by Skyaware

---
+++ CORE/ADSB/41_ADSB.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+###################
+# ADSB QRV MODULE #
+###################
+MODULE="ADSB"
+
+# STATION INFO
+source $HOME/.station-info
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+sudo unlink /usr/share/skyaware/html/mapnik
+if [ -d ${ARCOS_DATA}/QRV/OFFLINE-MAPS/viking/mapnik ]; then
+	sudo ln -s ${ARCOS_DATA}/QRV/OFFLINE-MAPS/viking/mapnik /usr/share/skyaware/html/mapnik
+fi
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
+
