44b329817f4824dba4241383ae8526d1ebdb0f89
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-11-28 15:05:12 +0000

    Add Gpredict CORE module

---
+++ CORE/GPREDICT/30_GPREDICT.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#######################
+# GPREDICT QRV MODULE #
+#######################
+MODULE="GPREDICT"
+
+# STATION INFO
+source $HOME/.station-info
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+SAVE_DIR=$ARCOS_DATA/QRV/$MYCALL/SAVED/$MODULE
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+mkdir -p $SAVE_DIR
+
+mkdir -p $SAVE_DIR/Gpredict
+unlink $HOME/.config/Gpredict
+rm -rf $HOME/.config/Gpredict
+ln -sTf $SAVE_DIR/Gpredict $HOME/.config/Gpredict
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || notify-send --icon=error "$MODULE" "$MODULE module failed!"
+
