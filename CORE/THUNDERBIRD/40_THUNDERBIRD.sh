42264e33b0202ef47a70fd5a9f98759fff3f0031
KG4VDK <146489609+kg4vdk@users.noreply.github.com>
2025-11-28 15:07:10 +0000

    Clean up 40_THUNDERBIRD.sh
    Removed commented-out code related to fs-based Thunderbird setup.

--- CORE/THUNDERBIRD/40_THUNDERBIRD.sh
+++ CORE/THUNDERBIRD/40_THUNDERBIRD.sh
@@ -25,26 +25,6 @@ unlink $HOME/.thunderbird
 rm -rf $HOME/.thunderbird
 ln -sTf $SAVE_DIR/thunderbird $HOME/.thunderbird
 
-#mkdir -p $SAVE_DIR
-#
-#if grep "thunderbird" /etc/mtab; then
-#	sudo umount $HOME/.thunderbird
-#fi
-#
-#rm -rf $HOME/.thunderbird
-#mkdir -p $HOME/.thunderbird
-#
-#if [ ! -f $SAVE_DIR/thunderbird-fs ]; then
-#	dd if=/dev/zero of=$SAVE_DIR/thunderbird-fs bs=1M count=1024
-#	mkfs.ext4 $SAVE_DIR/thunderbird-fs
-#	sudo mount $SAVE_DIR/thunderbird-fs $HOME/.thunderbird
-#	sudo chown user:user $HOME/.thunderbird
-#	sudo chmod 700 $HOME/.thunderbird
-#	sudo umount $HOME/.thunderbird
-#fi
-
-#sudo mount $SAVE_DIR/thunderbird-fs $HOME/.thunderbird
-
 } # END OF MODULE COMMANDS FUNCTION
 
 # Execute the module commands, and notify the user upon failure
