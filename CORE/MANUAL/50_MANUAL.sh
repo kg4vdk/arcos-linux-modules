d17bb81115ab19c0e9e930e0b22e11cb92cd6bd7
Mike Fisher <146489609+kg4vdk@users.noreply.github.com>
2026-04-10 17:55:19 +0000

    Re-add CORE module for field manual

---
+++ CORE/MANUAL/50_MANUAL.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#####################
+# MANUAL QRV MODULE #
+#####################
+MODULE="MANUAL"
+
+# STATION INFO
+source $HOME/.station-info
+
+# PATHS
+ARCOS_DATA=/arcHIVE
+MODULE_DIR=$ARCOS_DATA/QRV/$MYCALL/arcos-linux-modules/CORE/$MODULE
+LOGFILE=$MODULE_DIR/$MODULE.log
+########################
+
+### MODULE COMMANDS FUNCTION ###
+module_commands () {
+
+sudo cp $MODULE_DIR/arcOS-Field-Manual.html /opt/arcOS/
+cp $MODULE_DIR/arcOS-Field-Manual.desktop $HOME/.local/share/applications/
+
+} # END OF MODULE COMMANDS FUNCTION
+
+# Execute the module commands, and notify the user upon failure
+module_commands > $LOGFILE 2>&1 || echo "$MODULE" >> /tmp/.failed-modules.log
